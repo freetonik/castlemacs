a5edcf52ad3be32f03c891545f73fed237351072
Rakhim Davletkaliyev <freetonik@gmail.com>
2018-09-20 14:01:53 +0300

    remove unused header and update neotree settings

--- init.el
+++ init.el
@@ -218,13 +218,15 @@
   (setq neo-window-width 32
         neo-create-file-auto-open t
         neo-banner-message nil
-        neo-show-updir-line nil
+        neo-show-updir-line t
+        neo-window-fixed-size nil
+        neo-vc-integration nil
         neo-mode-line-type 'neotree
         neo-smart-open t
         neo-show-hidden-files t
         neo-mode-line-type 'none
         neo-auto-indent-point t)
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-theme (if (display-graphic-p) 'nerd 'arrow))
   (setq neo-hidden-regexp-list '("venv" "\\.pyc$" "~$" "\\.git" "__pycache__" ".DS_Store"))
   (global-set-key (kbd "s-B") 'neotree-toggle))           ;; Cmd+Shift+b toggle tree
 
