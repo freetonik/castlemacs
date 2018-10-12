c05e0d0ceb82f012f876c1ebb949c19618b55f6e
Rakhim Davletkaliyev <freetonik@gmail.com>
2018-10-12 11:38:04 +0300

    add info about private file

---
+++ early-init-private.el
@@ -0,0 +1,2 @@
+;; This is your private configuration file for pre-init things. It is loaded automatically, so feel free to add whatever you want.
+;; This file will not be affected by Castlemacs updates.
