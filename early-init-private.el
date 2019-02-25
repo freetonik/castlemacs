a2fbde2d14e1b0d8deaed839e72f3ae5b2f6577d
Adam Groves <adam.groves@gmail.com>
2019-02-25 20:28:03 +0100

    Remove private.el and early-init-private.el
    Updated the README to explicitly specify the path where private.el
    should be created.

--- early-init-private.el
+++
@@ -1,2 +0,0 @@
-;; This is your private configuration file for pre-init things. It is loaded automatically, so feel free to add whatever you want.
-;; This file will not be affected by Castlemacs updates.
